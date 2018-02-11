7efa043386b7105231ae892ef8ad44bd57399b50
iemektar <iemektar@outlook.com>
2018-02-11 10:29:35 +0300

    m

--- GPSTakip/settings.py
+++ GPSTakip/settings.py
@@ -142,8 +142,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+"""
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
-]
+]"""
