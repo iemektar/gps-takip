d5e71f7a73c5a972c2039d0ca77b8c42af7bece6
iemektar <iemektar@outlook.com>
2018-02-11 09:58:56 +0300

    m

--- GPSTakip/settings.py
+++ GPSTakip/settings.py
@@ -146,5 +146,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
-    '/GPSTakip/staticfiles'
+    '/staticfiles'
 ]
