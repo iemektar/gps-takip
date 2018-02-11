0d344ae5a0f54a8dca1b00f301119106b32119ac
iemektar <iemektar@outlook.com>
2018-02-11 10:00:40 +0300

    m

--- GPSTakip/settings.py
+++ GPSTakip/settings.py
@@ -146,5 +146,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
-    '/staticfiles'
 ]
--- GPSTakip/wsgi.py
+++ GPSTakip/wsgi.py
@@ -14,5 +14,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GPSTakip.settings")
 
-application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+#application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
