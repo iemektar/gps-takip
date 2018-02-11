929e5151882af3741561da328f4f5d6f98cd3289
iemektar <iemektar@outlook.com>
2018-02-11 09:54:42 +0300

    m

--- GPSTakip/settings.py
+++ GPSTakip/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')h=t!#*l3)7uktgk*$1oong@wrprdx!tsdlga996up=fn^ku6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
